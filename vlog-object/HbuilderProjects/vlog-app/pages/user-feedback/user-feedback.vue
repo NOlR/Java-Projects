7894d53879cfa9273908347e9b2fbb1e8a8b8bf5
NOlR <apple@appledeMacBook-Air.local>
2020-12-07 19:43:35 +0800

    个人资料编辑界面

--- vlog-object/HbuilderProjects/vlog-app/pages/user-feedback/user-feedback.vue
+++ vlog-object/HbuilderProjects/vlog-app/pages/user-feedback/user-feedback.vue
@@ -1,14 +1,26 @@
 <template>
 	<view>
-		<uni-popup>qwq</uni-popup>
+		<uni-collapse>
+			<uni-collapse-item title="标题" showAnimation>
+				<view class="content"><input type="text" placeholder="标题" class="border-bottom p-2"/></view>
+			</uni-collapse-item>
+			<uni-collapse-item title="内容" showAnimation>
+				<view class="content"><input type="text" placeholder="内容" class="border-bottom p-2"/></view>
+			</uni-collapse-item>
+		</uni-collapse>
+		<view class="py-2 px-3">
+			<button class="bg-main text-white" style="border-radius: 50rpx;border: 0;" type="primary">意见反馈</button>
+		</view>
 	</view>
 </template>
 
 <script>
-	import uniPopup from '@/components/uni-ui/uni-popup/uni-popup.vue';
+	import uniCollapseItem from '@/components/uni-ui/uni-collapse-item/uni-collapse-item.vue';
+	import uniCollapse from '@/components/uni-ui/uni-collapse/uni-collapse.vue';
 	export default {
-		comments:{
-			uniPopup
+		components:{
+			uniCollapseItem,
+			uniCollapse
 		},
 		data() {
 			return {
